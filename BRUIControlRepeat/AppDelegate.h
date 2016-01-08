7ca24ec32a98af4a334601a545e0f541da357c46
gitBurning <974201360@qq.com>
2016-01-08 11:14:44 +0800

    Initial Commit

---
+++ BRUIControlRepeat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRUIControlRepeat
+//
+//  Created by gitBurning on 16/1/8.
+//  Copyright © 2016年 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
